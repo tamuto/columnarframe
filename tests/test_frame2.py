d0a8cde109fc3ffdeeb975717f76d4774e443c39
tamuto <mutomob@gmail.com>
2021-05-28 17:07:50 +0900

    #10 行単位の削除対応

--- tests/test_frame2.py
+++ tests/test_frame2.py
@@ -11,3 +11,8 @@ class TestFrame2(unittest.TestCase):
             'col2': '5',
         })
         self.assertEqual(cf['col2'].to_list(), ['5', '5', '5'])
+
+    def test_remove(self):
+        cf = colf.read_csv('./data/test_data.csv')
+        cf2 = cf.remove(lambda x: x['col1'] == 'CCC')
+        self.assertEqual(cf2['col1'].to_list(), ['AAA', None, 'DDD'])
