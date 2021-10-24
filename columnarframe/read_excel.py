1530a7ed32fdf3528aea1c822676845eaca9a0cb
tamuto <mutomob@gmail.com>
2021-10-24 09:53:37 +0900

    #9 xls対応

--- columnarframe/read_excel.py
+++ columnarframe/read_excel.py
@@ -1,37 +1,46 @@
 from openpyxl import load_workbook
+from xlrd import open_workbook
+from xlrd.xldate import xldate_as_datetime
 
 from .frame import ColumnarFrame
 from .utils import skip_footer
 from .utils import make_key
 
 
-def convert(cell, datefmt):
-    if cell is not None and cell.value is None:
-        return None
-
-    if cell.data_type == 's':
-        return str(cell.value)
-    if cell.data_type == 'd':
-        if datefmt is None:
-            return str(cell.value)
-        return cell.value.strftime(datefmt)
-    if cell.data_type == 'n':
-        return str(cell.value)
-    if cell.data_type == 'b':
-        return str(cell.value)
-
-    raise RuntimeError('Unknown data type.')
-
-
 def read_excel(
     filename,
-    type='xlsx',
+    file_type='xlsx',
     sheet=None,
     skiprows=0,
     skipfooter=0,
     header=True,
     datefmt=None,
 ):
+    if file_type == 'xls':
+        return read_xls(filename, sheet, skiprows, skipfooter, header, datefmt)
+    if file_type == 'xlsx':
+        return read_xlsx(filename, sheet, skiprows, skipfooter, header, datefmt)
+    raise RuntimeError('Unknown file type.')
+
+
+def read_xlsx(filename, sheet, skiprows, skipfooter, header, datefmt):
+    def convert(cell, datefmt):
+        if cell is not None and cell.value is None:
+            return None
+
+        if cell.data_type == 's':
+            return str(cell.value)
+        if cell.data_type == 'd':
+            if datefmt is None:
+                return str(cell.value)
+            return cell.value.strftime(datefmt)
+        if cell.data_type == 'n':
+            return str(cell.value)
+        if cell.data_type == 'b':
+            return str(cell.value)
+
+        raise RuntimeError('Unknown data type.')
+
     wb = load_workbook(filename=filename)
     if sheet is None:
         ws = wb.active
@@ -50,6 +59,57 @@ def read_excel(
             key = str(idx)
 
         data[key] = [convert(c, datefmt) for c in rit]
+
+    data = skip_footer(data, skipfooter)
+
+    return ColumnarFrame(data)
+
+
+def read_xls(filename, sheet, skiprows, skipfooter, header, datefmt):
+    def convert(cell, datefmt, datemode):
+        if cell is not None and cell.value is None:
+            return None
+
+        if cell.ctype == 0:  # empty
+            return None
+        if cell.ctype == 1:  # string
+            return cell.value
+        if cell.ctype == 2:  # float
+            return str(cell.value)
+        if cell.ctype == 3:  # date
+            dt = xldate_as_datetime(cell.value, datemode)
+            if datefmt is None:
+                return str(dt)
+            return dt.strftime(datefmt)
+        if cell.ctype == 4:  # boolean
+            return 'True' if cell.value == 1 else 'False'
+        if cell.ctype == 5:  # error
+            return str(cell.value)
+        if cell.ctype == 6:  # blank
+            return None
+
+        raise RuntimeError('Unknown data type.')
+
+    wb = open_workbook(filename)
+
+    if sheet is None:
+        ws = wb.sheets()[0]
+    else:
+        ws = wb.sheet_by_name(sheet)
+
+    data = {}
+    for idx in range(ws.ncols):
+        columns = ws.col(idx)
+        if skiprows > 0:
+            rit = iter(columns[skiprows:])
+        else:
+            rit = iter(columns)
+        if header:
+            key = make_key(idx, next(rit).value)
+        else:
+            key = str(idx)
+
+        data[key] = [convert(c, datefmt, wb.datemode) for c in rit]
 
     data = skip_footer(data, skipfooter)
 
--- tests/test_read_excel.py
+++ tests/test_read_excel.py
@@ -37,20 +37,21 @@ class TestReadExcel(unittest.TestCase):
             ['2021-05-26', '2021-05-26', None, '2021-05-26']
         )
 
-    # def test_data_xls(self):
-    #     cf = colf.read_excel('./data/test_data.xls', file_type='xls')
-    #     self.assertEqual(len(cf), 5)
+    def test_data_xls(self):
+        cf = colf.read_excel('./data/test_data.xls', file_type='xls')
+        self.assertEqual(len(cf), 5)
 
-    # def test_header_xls(self):
-    #     # TODO
-    #     pass
+    def test_header_xls(self):
+        cf = colf.read_excel('./data/test_data.xls', file_type='xls', header=False)
+        self.assertEqual(len(cf), 6)
+        self.assertEqual(
+            cf['0'].to_list(),
+            ['col1', 'AAA', None, 'CCC', 'CCC', 'DDD'])
 
-    # def test_skiprows_xls(self):
-    #     cf = colf.read_excel(
-    #       './data/test_data.xls', file_type='xls', skiprows=2)
-    #     self.assertEqual(cf['0'].to_list(), ['CCC', 'CCC', 'DDD'])
+    def test_skiprows_xls(self):
+        cf = colf.read_excel('./data/test_data.xls', file_type='xls', skiprows=2)
+        self.assertEqual(cf['0'].to_list(), ['CCC', 'CCC', 'DDD'])
 
-    # def test_skipfooter_xls(self):
-    #     cf = colf.read_excel(
-    #       './data/test_data.xls', file_type='xls', skipfooter=2)
-    #     self.assertEqual(cf['col4'].to_list(), ['True', 'False', 'False'])
+    def test_skipfooter_xls(self):
+        cf = colf.read_excel('./data/test_data.xls', file_type='xls', skipfooter=2)
+        self.assertEqual(cf['col4'].to_list(), ['True', 'False', 'False'])
