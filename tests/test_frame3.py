0181f0da019004edf6d08d45c540182991ffe899
tamuto <mutomob@gmail.com>
2023-01-21 02:49:42 +0000

    dropメソッドの作成

--- tests/test_frame3.py
+++ tests/test_frame3.py
@@ -10,3 +10,10 @@ class TestFrame3(unittest.TestCase):
         sorted_cf = cf.sort(lambda x: str(x['col3']) + str(x['col1']))
 
         self.assertEqual(sorted_cf['col3'].to_list(), ['2020-05-03', '2020-05-25', '2020-07-03', '2021-05-03', None])
+
+
+    def test_drop(self):
+        cf = colf.read_csv('./data/test_data.csv')
+        drop_cf = cf.drop(['col1', 'col3'])
+
+        self.assertEqual(drop_cf.columns, ['col2', 'col4'])
